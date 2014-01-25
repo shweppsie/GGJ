8948bca0cfecd2b0c9d08973c888882fb60d03e1
Nathan Overall <git@shweppsie.com>
2014-01-25 15:17:07 +1300

    Add walking character and mouse press code

--- js/assets.js
+++ js/assets.js
@@ -4,9 +4,19 @@ function loadAssets() {
 			"scenes/"+scene+"/background.png",
 			"scenes/"+scene+"/far.png",
 			"scenes/"+scene+"/mid.png",
-			"sprites/"+scene+"/dude1.png",
-			"sprites/"+scene+"/dude2.png",
-			"sprites/"+scene+"/dude3.png",
+			"sprites/"+scene+"/idle1.png",
+			"sprites/"+scene+"/idle2.png",
+			"sprites/"+scene+"/idle3.png",
+			"sprites/"+scene+"/left1.png",
+			"sprites/"+scene+"/left2.png",
+			"sprites/"+scene+"/left3.png",
+			"sprites/"+scene+"/left4.png",
+			"sprites/"+scene+"/left5.png",
+			"sprites/"+scene+"/right1.png",
+			"sprites/"+scene+"/right2.png",
+			"sprites/"+scene+"/right3.png",
+			"sprites/"+scene+"/right4.png",
+			"sprites/"+scene+"/right5.png"
 		]
 	);
 	loader.onComplete = onAssetLoaded
--- js/globals.js
+++ js/globals.js
@@ -10,5 +10,7 @@ var sprite_animation_speed = 8;
 var player;
 var playerX = CANVAS_WIDTH/2;
 var playerY = CANVAS_HEIGHT-220;
-var player_frames = ["sprites/"+scene+"/dude1.png", "sprites/"+scene+"/dude2.png", "sprites/"+scene+"/dude3.png", "sprites/"+scene+"/dude2.png"];
+var player_idle_frames = ["sprites/"+scene+"/idle1.png", "sprites/"+scene+"/idle2.png", "sprites/"+scene+"/idle3.png"];
+var player_left_frames = ["sprites/"+scene+"/left1.png", "sprites/"+scene+"/left2.png", "sprites/"+scene+"/left3.png", "sprites/"+scene+"/left4.png", "sprites/"+scene+"/left5.png"];
+var player_right_frames = ["sprites/"+scene+"/right1.png", "sprites/"+scene+"/right2.png", "sprites/"+scene+"/right3.png", "sprites/"+scene+"/right4.png", "sprites/"+scene+"/right5.png"];
 var current_player_frame = 0;
