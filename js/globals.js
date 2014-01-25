834223e19cfa96f75e21ad7f9bc098c5101e8375
Nathan Overall <git@shweppsie.com>
2014-01-25 22:09:53 +1300

    Scaling, remove logging, frame count fix

--- js/globals.js
+++ js/globals.js
@@ -15,7 +15,7 @@ var sprite_animation_counter = 0;
 var sprite_animation_speed = 10; // lower = faster animation (don't judge me)
 var player;
 var playerX = CANVAS_WIDTH/2;
-var playerY = CANVAS_HEIGHT-220;
+var playerY = CANVAS_HEIGHT-140;
 var player_idle_frames = ["sprites/"+scene+"/idle1.png", "sprites/"+scene+"/idle2.png", "sprites/"+scene+"/idle3.png"];
 var player_left_frames = ["sprites/"+scene+"/left1.png", "sprites/"+scene+"/left2.png", "sprites/"+scene+"/left3.png", "sprites/"+scene+"/left4.png", "sprites/"+scene+"/left5.png"];
 var player_right_frames = ["sprites/"+scene+"/right1.png", "sprites/"+scene+"/right2.png", "sprites/"+scene+"/right3.png", "sprites/"+scene+"/right4.png", "sprites/"+scene+"/right5.png"];
